6d2a119e044757e518d3eefd7a780b1969f45d75
Stephen Gardner <stephenbgardner@gmail.com>
2018-10-14 16:55:58 -0700

    ft_bzero: no need to preserve register

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -6,7 +6,7 @@
 ;    By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2018/07/29 09:15:31 by sgardner          #+#    #+#              ;
-;    Updated: 2018/10/14 03:09:36 by sgardner         ###   ########.fr        ;
+;    Updated: 2018/10/14 16:54:45 by sgardner         ###   ########.fr        ;
 ;                                                                              ;
 ; ---------------------------------------------------------------------------- ;
 
@@ -16,12 +16,10 @@
 _ft_bzero:
 	test	rsi, rsi
 	je		.done
-	push	rsi
 .loop:
 	dec		rsi
 	mov		byte [rdi + rsi], 0
 	test	rsi, rsi
 	jne		.loop
-	pop		rsi
 .done:
 	ret
