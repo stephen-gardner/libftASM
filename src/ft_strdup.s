ec84a8e7006a60c8b99535b20ac0fedd5356aeae
Stephen Gardner <stephenbgardner@gmail.com>
2018-10-16 20:48:06 -0700

    ft_strdup: switch to stack space for vars

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -6,7 +6,7 @@
 ;    By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2018/10/16 05:35:39 by sgardner          #+#    #+#              ;
-;    Updated: 2018/10/16 19:36:57 by sgardner         ###   ########.fr        ;
+;    Updated: 2018/10/16 20:47:41 by sgardner         ###   ########.fr        ;
 ;                                                                              ;
 ; ---------------------------------------------------------------------------- ;
 
@@ -15,18 +15,21 @@
 
 	section	.text
 _ft_strdup:
-	push	rbp					; Align stack
-	mov		r12, rdi			; Source
+	push	rbp					; Save stack base pointer and align stack
+	mov		rbp, rsp
+	sub		rsp, 16				; Reserve space for local vars
+	mov		[rbp - 8], rdi		; Source
 	mov		al, 0
 	mov		rcx, -1
 	repne	scasb
 	mov		rdi, -1
 	sub		rdi, rcx
-	mov		r13, rdi			; Len
+	mov		[rbp - 16], rdi		; Len
 	call	_malloc
 	mov		rdi, rax
-	mov		rsi, r12
-	mov		rcx, r13
+	mov		rsi, [rbp - 8]
+	mov		rcx, [rbp - 16]
 	rep		movsb
+	add		rsp, 16
 	pop		rbp
 	ret
